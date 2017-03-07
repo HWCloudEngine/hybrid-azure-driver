c3dad7a2e2d8c9864997446727a1dd7f4ea50459
yanheven <yanheven@qq.com>
2017-03-07 18:56:51 +0800

    refactore cinder intreface
    use managed disk of Azure.
    
    Change-Id: Ic0a3991f1cf76099180909e75edb7bcd9dcb913a

--- cinder/tests/unit/volume/drivers/azure/test_adapter.py
+++ cinder/tests/unit/volume/drivers/azure/test_adapter.py
@@ -7,32 +7,21 @@ USERNAME = 'AZUREUSER'
 PASSWORD = 'PASSWORD'
 SUBSCRIBE_ID = 'ID'
 RG = 'RG'
-SAC = 'SC'
-KEY = 'KEY'
-
-
-class FakeKey(object):
-    class Value(object):
-        value = KEY
-    keys = [Value()]
 
 
 class AzureTestCase(TestCase):
 
     @mock.patch('cinder.volume.drivers.azure.adapter.UserPassCredentials')
-    @mock.patch('cinder.volume.drivers.azure.adapter.StorageManagementClient')
-    @mock.patch('cinder.volume.drivers.azure.adapter.CloudStorageAccount')
+    @mock.patch('cinder.volume.drivers.azure.adapter.ComputeManagementClient')
+    @mock.patch('cinder.volume.drivers.azure.adapter.ResourceManagementClient')
     def test_start_driver_with_user_password_subscribe_id(
-            self, cloudstorage, storage, credential):
-        storage().storage_accounts.list_keys = mock.Mock(return_value=FakeKey)
-        cloudstorage.create_page_blob_service = mock.Mock()
-
+            self, resource, compute, credential):
+        credentials = 'credentials'
+        credential.return_value = credentials
         azure = adapter.Azure(username=USERNAME,
-                              password=PASSWORD, subscription_id=SUBSCRIBE_ID,
-                              storage_account=SAC)
-
+                              password=PASSWORD, subscription_id=SUBSCRIBE_ID)
         credential.assert_called_once_with(USERNAME, PASSWORD)
-        storage.assert_called_with(credential(), SUBSCRIBE_ID)
-        cloudstorage.assert_called_once_with(account_name=SAC, account_key=KEY)
-        self.assertTrue(hasattr(azure, 'blob'))
-        self.assertTrue(hasattr(azure, 'storage'))
+        compute.assert_called_once_with(credentials, SUBSCRIBE_ID)
+        resource.assert_called_once_with(credentials, SUBSCRIBE_ID)
+        self.assertTrue(hasattr(azure, 'compute'))
+        self.assertTrue(hasattr(azure, 'resource'))
